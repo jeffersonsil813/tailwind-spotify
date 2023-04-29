fbdc9b023e8b7542a4ac13a3b03cbb2f820021ad
Jefferson Santos da Silva <jefferson.sil813@gmail.com>
2023-04-29 18:24:54 -0300

    Create suggestions

---
+++ src/components/Suggestions.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+
+interface SuggestionsProps {
+  imageSrc: string;
+  imageAlt: string;
+  title: string;
+  description: string;
+}
+
+export function Suggestions({
+  imageSrc,
+  imageAlt,
+  title,
+  description,
+}: SuggestionsProps) {
+  return (
+    <a
+      href="#"
+      className="bg-white/5 p-3 rounded-md hover:bg-white/10 flex flex-col gap-2"
+    >
+      <Image
+        className="w-full"
+        src={imageSrc}
+        alt={imageAlt}
+        width={120}
+        height={120}
+      />
+      <strong className="font-semibold">{title}</strong>
+      <span className="text-sm text-zinc-400">{description}</span>
+    </a>
+  );
+}
