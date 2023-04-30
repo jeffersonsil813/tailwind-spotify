1155c7c07137ec10ecdb8ff7acc67700b9e3c07f
Jefferson Santos da Silva <jefferson.sil813@gmail.com>
2023-04-29 23:12:35 -0300

    add 'overflow-y-auto' and use album componente

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,14 @@ import { Album } from "@/components/Album";
 import { Footer } from "@/components/Footer";
 import { Sidebar } from "@/components/Sidebar";
 import { Suggestions } from "@/components/Suggestions";
-import { ChevronLeft, ChevronRight, Play } from "lucide-react";
-import Image from "next/image";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 export default function Home() {
   return (
     <div className="h-screen flex flex-col">
-      <div className="flex flex-1">
+      <div className="flex flex-1 overflow-y-auto">
         <Sidebar />
-        <main className="flex-1 p-6">
+        <main className="flex-1 p-6 overflow-y-auto">
           <div className="flex items-center gap-3">
             <button className="rounded-full bg-black/40 p-1">
               <ChevronLeft />
@@ -20,89 +19,53 @@ export default function Home() {
             </button>
           </div>
           <h1 className="font-semibold text-3xl mt-10">Good Afternoon</h1>
-          <div className="grid grid-cols-3 gap-4 mt-4">
+          <div className="flex flex-col lg:grid lg:grid-cols-3 gap-4 mt-4">
             <Album
-              imageSrc="/album.jpg"
-              imageAlt="Capa do álbum da Hillsong"
+              imageSrc="/images/open-heaven-album.jpg"
+              imageAlt="Álbum Open Heaven"
               title="Open Heaven"
             />
             <Album
-              imageSrc="/album.jpg"
-              imageAlt="Capa do álbum da Hillsong"
-              title="Open Heaven"
+              imageSrc="/images/this-is-our-god-album.jpg"
+              imageAlt="Álbum This Is Our God"
+              title="This Is Our God"
             />
             <Album
-              imageSrc="/album.jpg"
-              imageAlt="Capa do álbum da Hillsong"
-              title="Open Heaven"
+              imageSrc="/images/Beerbongs-Bentleys.png"
+              imageAlt="Álbum Beerbongs & Bentleys"
+              title="Beerbongs & Bentleys"
             />
             <Album
-              imageSrc="/album.jpg"
-              imageAlt="Capa do álbum da Hillsong"
-              title="Open Heaven"
+              imageSrc="/images/Legends_Never_Die.png"
+              imageAlt="Álbum Legends Never Die"
+              title="Legends Never Die"
             />
             <Album
-              imageSrc="/album.jpg"
-              imageAlt="Capa do álbum da Hillsong"
-              title="Open Heaven"
+              imageSrc="/images/Beauty_Behind_the_Madness.png"
+              imageAlt="Álbum Beauty Behind the Madness"
+              title="Beauty Behind the Madness"
             />
             <Album
-              imageSrc="/album.jpg"
-              imageAlt="Capa do álbum da Hillsong"
-              title="Open Heaven"
+              imageSrc="/images/Teus_Sonhos_Fernandinho.jpg"
+              imageAlt="Álbum Teus Sonhos"
+              title="Teus Sonhos"
             />
           </div>
           <h2 className="font-semibold text-2xl mt-10">
             Made for Jefferson Santos da Silva
           </h2>
-          <div className="grid grid-cols-8 gap-4 mt-4">
+          <div className="flex flex-col lg:grid lg:grid-cols-6 gap-4 mt-4">
             <Suggestions
-              imageSrc="/album.jpg"
-              imageAlt="Capa do álbum da Hillsong"
-              title="Title"
-              description="Artists, famous, genius, music, songs, worship"
+              imageSrc="/images/Hollywood's_Bleeding.png"
+              imageAlt="Álbum Hollywood's Bleeding"
+              title="Hollywood's Bleeding"
+              description="Lorem ipsum dolor sit amet."
             />
             <Suggestions
-              imageSrc="/album.jpg"
-              imageAlt="Capa do álbum da Hillsong"
-              title="Title"
-              description="Artists, famous, genius, music, songs, worship"
-            />
-            <Suggestions
-              imageSrc="/album.jpg"
-              imageAlt="Capa do álbum da Hillsong"
-              title="Title"
-              description="Artists, famous, genius, music, songs, worship"
-            />
-            <Suggestions
-              imageSrc="/album.jpg"
-              imageAlt="Capa do álbum da Hillsong"
-              title="Title"
-              description="Artists, famous, genius, music, songs, worship"
-            />
-            <Suggestions
-              imageSrc="/album.jpg"
-              imageAlt="Capa do álbum da Hillsong"
-              title="Title"
-              description="Artists, famous, genius, music, songs, worship"
-            />
-            <Suggestions
-              imageSrc="/album.jpg"
-              imageAlt="Capa do álbum da Hillsong"
-              title="Title"
-              description="Artists, famous, genius, music, songs, worship"
-            />
-            <Suggestions
-              imageSrc="/album.jpg"
-              imageAlt="Capa do álbum da Hillsong"
-              title="Title"
-              description="Artists, famous, genius, music, songs, worship"
-            />
-            <Suggestions
-              imageSrc="/album.jpg"
-              imageAlt="Capa do álbum da Hillsong"
-              title="Title"
-              description="Artists, famous, genius, music, songs, worship"
+              imageSrc="/images/attention.jpg"
+              imageAlt="Álbum Atenção Banda Morada"
+              title="Atenção"
+              description="Lorem ipsum dolor sit amet."
             />
           </div>
         </main>
