7af6cf24b073f61646049149337a87c13f1afcb3
Jefferson Santos da Silva <jefferson.sil813@gmail.com>
2023-04-29 23:08:30 -0300

    add space in album content

--- src/components/Album.tsx
+++ src/components/Album.tsx
@@ -13,9 +13,11 @@ export function Album({ imageSrc, imageAlt, title }: AlbumProps) {
       href="#"
       className="bg-white/5 group rounded overflow-hidden flex items-center gap-4 hover:bg-white/10 transition-colors"
     >
-      <Image src={imageSrc} alt={imageAlt} width={100} height={100} />
-      <strong>{title}</strong>
-      <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+      <div className="flex items-center flex-1 gap-4">
+        <Image src={imageSrc} alt={imageAlt} width={100} height={100} />
+        <strong>{title}</strong>
+      </div>
+      <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black mr-6 invisible group-hover:visible">
         <Play />
       </button>
     </a>
