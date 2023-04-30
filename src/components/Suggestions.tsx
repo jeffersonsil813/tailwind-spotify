323854943f9b5f8c80e403398cb34b09cd33ad87
Jefferson Santos da Silva <jefferson.sil813@gmail.com>
2023-04-29 23:05:03 -0300

    add 'break-words' on suggestions

--- src/components/Suggestions.tsx
+++ src/components/Suggestions.tsx
@@ -16,7 +16,7 @@ export function Suggestions({
   return (
     <a
       href="#"
-      className="bg-white/5 p-3 rounded-md hover:bg-white/10 flex flex-col gap-2 transition-colors"
+      className="bg-white/5 p-3 rounded-md hover:bg-white/10 flex flex-col gap-2 transition-colors break-words"
     >
       <Image
         className="w-full"
